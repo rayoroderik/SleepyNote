bf0d4ec4e9b340a2b64a0b796fffc8ad473cdf3f
Rayo Roderik <rayoroderik@gmail.com>
2018-10-10 11:39:35 +0700

    Initial Commit

---
+++ SleepyNote/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SleepyNote
+//
+//  Created by Rayo Roderik on 10/10/18.
+//  Copyright © 2018 Rayo Roderik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
